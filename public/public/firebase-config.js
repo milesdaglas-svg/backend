12e34817c2d182f42549e129b9e1eccf159e86d5
milesdaglas-svg <milesdaglas@gmail.com>
2026-06-14 18:40:40 +0300

    Auto update

---
+++ public/public/firebase-config.js
@@ -0,0 +1,13 @@
+/* =========================
+   GLOBAL FIREBASE CONFIG
+   Baked in — works for ALL users
+   automatically, no setup needed
+========================= */
+window.GLOBAL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAyQ5n7QuAaa6S4q3u6qpSXYZhZZ6J7WWM",
+  authDomain: "vsc-clone.firebaseapp.com",
+  projectId: "vsc-clone",
+  storageBucket: "vsc-clone.firebasestorage.app",
+  messagingSenderId: "804651890113",
+  appId: "1:804651890113:web:ee924c6a8c3995b40bab40"
+};
